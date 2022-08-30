cfd93f4d0335043f71dfd6e1490435eadf76b715
Tom Wilson <tom@hyper.io>
2022-08-30 13:37:14 +0000

    update reward script to use warp 1.2

---
+++ reward.mjs
@@ -0,0 +1,19 @@
+import { WarpFactory } from 'warp-contracts/mjs'
+import fs from 'fs'
+
+const walletFile = process.argv.slice(2)[0]
+const wallet = JSON.parse(fs.readFileSync(walletFile, 'utf-8'))
+
+const CONTRACT = 'aSMILD7cEJr93i7TAVzzMjtci_sGkXcWnqpDkG6UGcA'
+const warp = WarpFactory.forMainnet()
+const contract = warp.contract(CONTRACT).connect(wallet).setEvaluationOptions({
+  allowUnsafeClient: true,
+  allowBigInt: true
+})
+
+const result = await contract.writeInteraction({
+  function: 'reward',
+  timestamp: Date.now()
+})
+
+console.log('result', result)
