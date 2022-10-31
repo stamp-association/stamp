465a9c66cb11451a663d2091bdc8f1f9f04dcddf
Tom Wilson <tom@hyper.io>
2022-10-31 14:29:20 +0000

    stage evolve contract

--- evolve.mjs
+++ evolve.mjs
@@ -1,6 +1,6 @@
 // deploy contract to warp
 import Arweave from 'arweave'
-import { WarpNodeFactory } from 'warp1'
+import { WarpFactory } from 'warp-contracts'
 import fs from 'fs'
 
 const arweave = Arweave.init({
@@ -9,13 +9,13 @@ const arweave = Arweave.init({
   protocol: 'https'
 })
 
-const warp = WarpNodeFactory.memCached(arweave)
+const warp = WarpFactory.forMainnet()
 
 const walletFile = process.argv.slice(2)[0]
 const wallet = JSON.parse(fs.readFileSync(walletFile, 'utf-8'))
 const src = fs.readFileSync('./dist/contract.js', 'utf-8')
 //const creator = '4ALXfd76F129U8OCv0YUzTSuBTivUeqAVqnLD-sUk4c'
-const CONTRACT = 'aSMILD7cEJr93i7TAVzzMjtci_sGkXcWnqpDkG6UGcA' // STAMPCOIN-TEST-v10
+const CONTRACT = 'FMRHYgSijiUNBrFy-XqyNNXenHsCV0ThR4lGAPO4chA' // STAMPCOIN-TEST-v12
 const contract = warp.contract(CONTRACT).connect(wallet)
 
 const srcTxId = await contract.save({ src, useBundler: false })
