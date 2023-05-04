8cc6263699e73d184ac35d78f44bc751d955dedd
Tom Wilson <tom@hyper.io>
2023-05-04 15:46:53 +0000

    refactor: working on stamp v1 contract wip #12

--- warp/src/__tests__/integration/_helpers.ts
+++
@@ -1,5 +0,0 @@
-import { Warp } from '@warp';
-
-export async function mineBlock(warp: Warp) {
-  await warp.testing.mineBlock();
-}
--- warp/src/index.ts
+++
@@ -1,9 +0,0 @@
-// note: all of the aliases are replaced by relative paths after compilation
-// - using https://www.npmjs.com/package/tsc-alias plugin
-export * from '@warp/logging'; // this needs to be the first exported element.
-export * from '@warp/core';
-export * from '@warp/contract';
-export * from '@warp/cache';
-export * from '@warp/plugins';
-export * from '@warp/legacy';
-export * from '@warp/utils';
