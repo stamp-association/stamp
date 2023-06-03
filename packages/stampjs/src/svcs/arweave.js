368ecd3d5e7bd77aed15a705f26b75e7c6bc7396
Tom Wilson <tom@hyper.io>
2023-06-03 16:34:33 +0000

    fix stamp counts

--- packages/stampjs/src/svcs/arweave.js
+++ packages/stampjs/src/svcs/arweave.js
@@ -30,6 +30,11 @@ export const vouchServices = () =>
   fetch(
     "https://dre-1.warp.cc/contract/?id=_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk&query=$.votes"
   )
+    .catch(_ => fetch("https://dre-5.warp.cc/contract/?id=_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk&query=$.votes"))
+    .catch(_ => fetch("https://dre-6.warp.cc/contract/?id=_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk&query=$.votes"))
+    .catch(_ => fetch("https://dre-4.warp.cc/contract/?id=_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk&query=$.votes"))
+    .catch(_ => fetch("https://dre-3.warp.cc/contract/?id=_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk&query=$.votes"))
+    .catch(_ => fetch("https://dre-2.warp.cc/contract/?id=_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk&query=$.votes"))
     .then((r) => r.json())
     .then(({ result }) => result[0].filter((v) => v.key === "Voucher"))
     .then((vouchedVotes) => vouchedVotes.map((v) => v.value));
